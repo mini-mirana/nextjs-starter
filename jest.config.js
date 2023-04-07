68f92d015f43c8414fb893b5d79fe247ba9c74c3
mini-mirana <mini_mirana@yahoo.com>
2023-04-07 15:14:49 +0200

    fix: :bug: fix test (playwrite & jest) configs

--- jest.config.js
+++ jest.config.js
@@ -8,11 +8,14 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
+  // Handle module aliases
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
   // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
-  moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom'
+  testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['<rootDir>/__tests__/e2e/']
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
